3609e092826157addf4f4ee082d18406415bfbc5
fairysu <370699997@qq.com>
2017-05-20 12:29:28 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,9 +2,9 @@ from qqbot import QQBotSlot as qqbotslot, RunBot
 
 @qqbotslot
 def onQQMessage(bot, contact, member, content):
-    if content == '-hello':
-        bot.SendTo(contact, '你好，我是QQ机器人')
-    elif content == '-stop':
+    if content == 'hello':
+        bot.SendTo(contact, 'HELLO')
+    elif content == 'stop':
         bot.SendTo(contact, 'QQ机器人已关闭')
         bot.Stop()
 
